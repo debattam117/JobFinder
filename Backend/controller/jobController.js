dc01ae8ae58fb54a477d09a9dd9e1614d871d0bc
Debattam Bhattacharjee <74034972+debattam117@users.noreply.github.com>
2024-08-05 13:54:45 +0530

    Delete Update Fetch
    Write logic for
    
    GET  Job details posted by specific user
    DELETE  Job details posted by specific user
    UPDATE job details posted by specific user

--- Backend/controller/jobController.js
+++ Backend/controller/jobController.js
@@ -3,6 +3,8 @@ import { ErrorHandler } from "../middleware/error.js"
 import { Job } from "../model/jobSchema.js";
 import { sendToken } from "../util/jwtToken.js";
 
+
+//Show all the jobs present
 export const getAllJobs=catchAsyncError(async (req,res,next)=>{
     const jobs= await Job.find({expired: false});
     res.status(200).json({
@@ -12,8 +14,10 @@ export const getAllJobs=catchAsyncError(async (req,res,next)=>{
 
 });
 
+
+//POST job  details in the Data Base
 export const postJobs=catchAsyncError(async(req,res,next)=>{
-    const{role}=req.user;
+    const{role}=req.user;     //req.user we are getting auth.js i.e isAuthorized i.e we have used in routes and then its coming here as req
     if(role ==="Job Seeker"){
         return next(new ErrorHandler
         ("Job Seeker is not allowed to access this resource!",400)
@@ -58,4 +62,81 @@ export const postJobs=catchAsyncError(async(req,res,next)=>{
       job
     });
 
+});
+
+//GET  Job details posted by specific user
+export const getMyJobs= (async (req,res,next)=>{
+    const{role}=req.user;
+    if(role ==="Job Seeker"){
+        return next(new ErrorHandler
+        ("Job Seeker is not allowed to access this resource!",400)
+    )};
+
+    const jobs = await Job.find({
+        $and: [
+            { expired: false },
+            { postedBy: req.user._id }
+        ]
+    });
+
+    res.status(200).json({
+        success: true,
+        jobs
+    });
+
+});
+
+
+//PUT: Update Job details posted by specific user
+export const updateJob= (async (req,res,next)=>{
+    const{role}=req.user;
+    if(role ==="Job Seeker"){
+        return next(new ErrorHandler
+        ("Job Seeker is not allowed to access this resource!",400)
+    )};
+
+    const{id}=req.params;              //here id is same as we used in routes. router.put("/update/:id",isAuthorized, updateJob);
+
+    let job=await Job.findById(id);
+
+    if(!job){
+        return next(new ErrorHandler
+        ("Oops Job not found!",404)
+    )};
+
+    job = await Job.findByIdAndUpdate(id,req.body,{
+        new:true,
+        runValidators:true,
+        userFindAndModify:false
+    });
+    res.status(200).json({
+        success:true,
+        job,
+        message:"Job updated Successfully!"
+    });
+});
+
+export const deletJobs= (async (req,res,next)=>{
+
+    const{role}=req.user;
+    if(role ==="Job Seeker"){
+        return next(new ErrorHandler
+        ("Job Seeker is not allowed to access this resource!",400)
+    )};
+
+    const{id}=req.params;              
+
+    let job=await Job.findById(id);
+
+    if(!job){
+        return next(new ErrorHandler
+        ("Oops Job not found!",404)
+    )};
+
+    await job.deleteOne();
+
+    res.status(200).json({
+        success:true,
+        message:"Job deleted Successfully!"
+    });
 });
--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getAllJobs,postJobs } from "../controller/jobController.js";
+import { getAllJobs,postJobs,getMyJobs,updateJob,deletJobs } from "../controller/jobController.js";
 import isAuthorized from "../middleware/auth.js";
 
 const router=express.Router();
 
-router.get("/getAllJobs",isAuthorized, getAllJobs);
+router.get("/getAllJobs", getAllJobs);
 router.post("/postJobs",isAuthorized ,postJobs);
+router.get("/getMyJobs",isAuthorized, getMyJobs);
+router.put("/update/:id",isAuthorized, updateJob);
+router.delete("/Delete/:id",isAuthorized, deletJobs);
+
+updateJob
 
 export default router;

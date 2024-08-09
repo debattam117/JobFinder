9a7af9b3a0f21c5f5ef86fd6f8a6690c3978aa30
Debattam Bhattacharjee <74034972+debattam117@users.noreply.github.com>
2024-08-09 13:51:34 +0530

    Added routes in app.js
    Added route in app.js
    created a fetchuser sync funcytion
    installed some node package .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import {React, useContext, useEffect} from 'react';
+import axios from 'axios';
+import Toaster from 'react-hot-toast';
+import {Context} from './index.js';
+import { Routes,Route } from 'react-router-dom';
+import './App.css';
+import Login from './components/Auth/Login.js';
+import Register from './components/Auth/Register.js';
+import Navbar from './components/layout/Navbar.js';
+import Footer from './components/layout/Footer.js';
+import Home from './components/Home/Home.js';
+import Jobs from './components/job/Jobs.js';
+import JobDetails from './components/job/JobDetails.js';
+import MyJobs from './components/job/MyJobs.js';
+import PostJob from './components/job/PostJob.js';
+import Application from './components/Applications/Application.js';
+import MyApplication from './components/Applications/MyApplication.js';
+import NotFound from './components/NotFound/NotFound.js';
+
+
 
 const App = () => {
+
+
+const {isAuthorized,setIsAuthorized,user,setUser}=useContext(Context);
+
+useEffect(()=>{
+  const fetchUser = async ()=>{
+    try {
+      const response= axios.get("",{withCredentials:true});
+      setUser(response.data.user);
+      setIsAuthorized(true);
+    } catch (error) {
+      setIsAuthorized(false);
+    }
+  };
+  fetchUser();
+  },[isAuthorized]);
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+  <>
+
+    <Navbar/>
+
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/Login" element={<Login/>}/>
+    <Route path="/Register" element={<Register/>}/>
+    <Route path="/job/getAll" element={<Jobs/>}/>
+    <Route path="job/:id" element={<JobDetails/>}/>
+    <Route path="job/post" element={<PostJob/>}/>
+    <Route path="job/me" element={<MyJobs/>}/>
+    <Route path="application/:id" element={<Application/>}/>
+    <Route path="application/me" element={<MyApplication/>}/>
+    <Route path="*" element={<NotFound/>}/> {/*when we are using * in path that means when there is no path define by default it will redirect to the star path */}
+    </Routes>
+
+    <Footer/>
+    <Toaster/>
+
+  </>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createContext } from "react";
+import { BrowserRouter } from 'react-router-dom';
+
+export const Context=createContext({isAuthorized:false});
+
+const AppWrapper=()=>{
+   const[isAuthorized,setIsAuthorized]=useState(false);
+   const[user,setUser]=useState({});
+
+   return(
+    <Context.Provider value={{isAuthorized,setIsAuthorized,user,setUser}}>
+    <App />
+    </Context.Provider>
+   )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
-    <App />
+    <AppWrapper/>
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

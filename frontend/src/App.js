24563132be0453bb7549271495e0cef56015e364
Debattam Bhattacharjee <74034972+debattam117@users.noreply.github.com>
2024-08-12 13:42:40 +0530

    Added code for NAVBAR

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import {React, useContext, useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
 import Toaster from 'react-hot-toast';
-import {Context} from './index.js';
-import { Routes,Route } from 'react-router-dom';
+import { Context } from './index.js';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Auth/Login.js';
 import Register from './components/Auth/Register.js';
@@ -17,53 +17,116 @@ import Application from './components/Applications/Application.js';
 import MyApplication from './components/Applications/MyApplication.js';
 import NotFound from './components/NotFound/NotFound.js';
 
-
-
 const App = () => {
+  // eslint-disable-next-line
+  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
 
-
-const {isAuthorized,setIsAuthorized,user,setUser}=useContext(Context);
-
-useEffect(()=>{
-  const fetchUser = async ()=>{
-    try {
-      const response= axios.get("http://localhost:4001/api/v1/user/getUserDetails",{withCredentials:true});
-      setUser(response.data.user);
-      setIsAuthorized(true);
-    } catch (error) {
-      setIsAuthorized(false);
-    }
-  };
-  fetchUser();
-  },[isAuthorized]);
-
-  // if(isAuthorized){
-  //   return <Navigate to={}/>
-  // }
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:4001/api/v1/user/getUserDetails", { withCredentials: true });
+        setUser(response.data.user);
+        setIsAuthorized(true);
+      } catch (error) {
+        setIsAuthorized(false);
+      }
+    };
+    fetchUser();
+  }, [setIsAuthorized, setUser]);
 
   return (
-  <>
+    <>
+      <Navbar />
 
-    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/job/getAll" element={<Jobs />} />
+        <Route path="job/:id" element={<JobDetails />} />
+        <Route path="job/post" element={<PostJob />} />
+        <Route path="job/me" element={<MyJobs />} />
+        <Route path="application/:id" element={<Application />} />
+        <Route path="application/me" element={<MyApplication />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
 
-    <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/Login" element={<Login/>}/>
-    <Route path="/Register" element={<Register/>}/>
-    <Route path="/job/getAll" element={<Jobs/>}/>
-    <Route path="job/:id" element={<JobDetails/>}/>
-    <Route path="job/post" element={<PostJob/>}/>
-    <Route path="job/me" element={<MyJobs/>}/>
-    <Route path="application/:id" element={<Application/>}/>
-    <Route path="application/me" element={<MyApplication/>}/>
-    <Route path="*" element={<NotFound/>}/> {/*when we are using * in path that means when there is no path define by default it will redirect to the star path */}
-    </Routes>
+      <Footer />
+      <Toaster />
+    </>
+  );
+};
 
-    <Footer/>
-    <Toaster/>
+export default App;
 
-  </>
-  )
-}
 
-export default App
+
+// import {React, useContext, useEffect} from 'react';
+// import axios from 'axios';
+// import Toaster from 'react-hot-toast';
+// import {Context} from './index.js';
+// import { Routes,Route } from 'react-router-dom';
+// import './App.css';
+// import Login from './components/Auth/Login.js';
+// import Register from './components/Auth/Register.js';
+// import Navbar from './components/layout/Navbar.js';
+// import Footer from './components/layout/Footer.js';
+// import Home from './components/Home/Home.js';
+// import Jobs from './components/job/Jobs.js';
+// import JobDetails from './components/job/JobDetails.js';
+// import MyJobs from './components/job/MyJobs.js';
+// import PostJob from './components/job/PostJob.js';
+// import Application from './components/Applications/Application.js';
+// import MyApplication from './components/Applications/MyApplication.js';
+// import NotFound from './components/NotFound/NotFound.js';
+
+
+
+// const App = () => {
+
+
+// const {isAuthorized,setIsAuthorized,user,setUser}=useContext(Context);
+
+// useEffect(()=>{
+//   const fetchUser = async ()=>{
+//     try {
+//       const response= axios.get("http://localhost:4001/api/v1/user/getUserDetails",{withCredentials:true});
+//       setUser(response.data.user);
+//       setIsAuthorized(true);
+//     } catch (error) {
+//       setIsAuthorized(false);
+//     }
+//   };
+//   fetchUser();
+//   },[isAuthorized]);
+
+//   // if(isAuthorized){
+//   //   return <Navigate to={}/>
+//   // }
+
+//   return (
+//   <>
+
+//     <Navbar/>
+
+//     <Routes>
+//     <Route path="/" element={<Home/>}/>
+//     <Route path="/Login" element={<Login/>}/>
+//     <Route path="/Register" element={<Register/>}/>
+//     <Route path="/job/getAll" element={<Jobs/>}/>
+//     <Route path="job/:id" element={<JobDetails/>}/>
+//     <Route path="job/post" element={<PostJob/>}/>
+//     <Route path="job/me" element={<MyJobs/>}/>
+//     <Route path="application/:id" element={<Application/>}/>
+//     <Route path="application/me" element={<MyApplication/>}/>
+//     <Route path="*" element={<NotFound/>}/> {/*when we are using * in path that means when there is no path define by default it will redirect to the star path */}
+//     </Routes>
+
+//     <Footer/>
+//     <Toaster/>
+
+//   </>
+//   )
+// }
+
+// export default App
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -3,6 +3,8 @@ import {Context} from '../../index.js';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import { Link } from 'react-router-dom';
+import {GiHamburgerMenu} from 'react-icons/gi';
 
 const Navbar = () => {
 
@@ -24,7 +26,41 @@ const Navbar = () => {
   return (
     <>
     <nav className={isAuthorized?"navbarShow":"navbarHide"}>
-      
+      <div className='Container'>
+       <div className='logo'>
+        <img src='' alt='logo'></img>
+       </div>
+       <ul className={!show?"menu":"show-menu menu"}>
+        <li>
+          <Link to={"/"} onClick={()=>setShow(false)}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/job/getall"} onClick={()=>setShow(false)}>All JOBS</Link>
+        </li>
+        <li>
+          <Link to={"/application/me"} onClick={()=>setShow(false)}>
+           {
+             user && user.role === "Employer" ? "APPLICATIONS":"MY APPLICATION"
+           }
+          </Link>
+        </li>
+        {user && user.role=== "Employer"?(
+         <>
+            <li>
+              <Link to={"/job/post"}  onClick={()=>setShow(false)} >POST NEW JOBS</Link>
+            </li>
+            <li>
+              <Link to={"/job/me"}  onClick={()=>setShow(false)} >VIEW YOUR JOBS</Link>
+            </li>
+         </>
+         ):(<></>)
+         }
+         <button onClick={handleLogout}>LOGOUT</button>
+       </ul>
+       <div className='Hamburger'>
+        <GiHamburgerMenu onClick={()=>setShow(!show)}/>
+       </div>
+      </div>
     </nav>
     </>
   )
